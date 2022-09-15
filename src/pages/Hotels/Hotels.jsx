68a289fb4ee84adff94baa3e29c590777da1ca31
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-09-15 20:27:19 +0300

    feat: page login

--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 function Hotels() {
   return (
-    <div>Hotels</div>
+    <main className='hotels-page'>
+      <header className='header'> header</header>
+      <aside className='aside'>
+        <div className='search-form'>
+          поисковая форма
+        </div>
+        <div className='favourites'>
+          избранное
+        </div>
+      </aside>
+      <div className='hotels'>
+        {/* <h1></h1> */}
+      </div>
+    </main>
   );
 }
 
