8aea954175aaa9b4bb8e6935b8e056eaca0f2daf
Egor Kurkin <proehavshiynapravo@yandex.ru>
2022-10-07 14:36:08 +0300

    refactor: Input component

--- src/components/UI/FormFieldset/FormFieldset.jsx
+++ src/components/UI/FormFieldset/FormFieldset.jsx
@@ -1,53 +1,30 @@
+/* eslint-disable import/order */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import classNames from 'classnames/bind';
-import { ru } from 'date-fns/esm/locale';
-import DatePicker from 'react-datepicker';
 
 import styles from './FormFieldset.module.scss';
-import './datepicker.css';
+import Input from '../Input/Input';
 
 const cn = classNames.bind(styles);
 
 function FormFieldset({
   labelPlaceholder = 'Поле', type = 'text', name, id, value, onChange, onBlur, isTouched, errorMessage,
 }) {
-  function handleChangeDatePicker(pickedDate) {
-    const e = {
-      target: {
-        value: new Date(pickedDate).getTime(),
-      },
-    };
-    onChange(e);
-  }
-
   return (
     <fieldset className={cn('fieldset')}>
       <label htmlFor={id} className={cn('label', { label_error: isTouched && errorMessage })}>
         {labelPlaceholder}
       </label>
-      <input
+      <Input
         type={type}
         name={name}
         id={id}
-        className={cn('input', { input_error: isTouched && errorMessage })}
         value={value}
         onChange={onChange}
         onBlur={onBlur}
+        isError={isTouched && errorMessage}
       />
-      {type === 'date'
-        ? (
-          <DatePicker
-            customInput={<button className={cn('datepicker')} type='button' aria-label='pick-date' />}
-            selected={new Date(value)}
-            onChange={handleChangeDatePicker}
-            locale={ru}
-            minDate={Date.now()}
-            todayButton='Сегодня'
-            closeOnScroll
-            popperPlacement='top-end'
-          />
-        )
-        : null}
       <span className={cn('input-error', { 'input-error_active': isTouched && errorMessage })}>
         {errorMessage}
       </span>
---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import { ru } from 'date-fns/esm/locale';
+import DatePicker from 'react-datepicker';
+
+import styles from './Input.module.scss';
+import './datepicker.css';
+
+const cn = classNames.bind(styles);
+
+function Input({
+  type = 'text', name, id, value, onChange, onBlur, isError,
+}) {
+  function handleChangeDatePicker(pickedDate) {
+    const e = {
+      target: {
+        value: new Date(pickedDate).getTime(),
+      },
+    };
+    onChange(e);
+  }
+
+  return (
+    <>
+      <input
+        type={type}
+        name={name}
+        id={id}
+        className={cn('input', { input_error: isError })}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {type === 'date'
+        ? (
+          <DatePicker
+            customInput={<button className={cn('datepicker-icon')} type='button' aria-label='pick-date' />}
+            selected={new Date(value)}
+            onChange={handleChangeDatePicker}
+            locale={ru}
+            minDate={Date.now()}
+            todayButton='Сегодня'
+            closeOnScroll
+            popperPlacement='top-end'
+          />
+        )
+        : null}
+    </>
+  );
+}
+
+export default Input;
